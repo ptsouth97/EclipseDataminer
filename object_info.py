341ba94dd7607df7dbcd945a3e2ffb9c3fe7b1ff
Blake Crosby <ptsouth97@gmail.com>
2018-05-23 21:52:52 -0400

    updated object_info.py

--- object_info.py
+++ object_info.py
@@ -6,82 +6,86 @@ from sys import platform
 import requests
 from bs4 import BeautifulSoup
 
+
 def main():
-    ''' main loop for testing this module only'''
-    nme = 'field=BUL_SC7&starid=362538&db=DIA&points=good'
-    get_data_from_web(nme)
+	''' main loop for testing this module only'''
+
+	nme = 'field=BUL_SC7&starid=362538&db=DIA&points=good'
+	get_data_from_web(nme)
 
 
 def set_name(field, star_id):
-    '''store basic information about the object of interest'''
+	'''store basic information about the object of interest'''
     
-    # url_id = field.lower() + '_i_' + star_id + '.dat'
-    path_id = 'OGLEII_' + field + '_' + star_id
-    url_id = 'field={}&starid={}&db=DIA&points=good'.format(field, star_id)
+	# url_id = field.lower() + '_i_' + star_id + '.dat'
+	path_id = 'OGLEII_' + field + '_' + star_id
+	url_id = 'field={}&starid={}&db=DIA&points=good'.format(field, star_id)
 
-    print('You entered: ' + path_id + ' and the url_id is ' + url_id)
-    print('')
+	print('The ID is: ' + path_id + ' and the url_id is ' + url_id)
+	print('')
     '''correct = input('Is this correct?  [1]=Yes, [any other key]=No ').strip()
        if correct == '1':
        break'''
-    return path_id, url_id
+	return path_id, url_id
 
 
 def make_folder(path_id):
+	'''set up a new folder for the object's files and create a new dataframe to store the object info'''
 	
-    if platform == 'linux':
-        newpath = os.getcwd() + '/' + path_id
-    else:
-        newpath = os.getcwd() + '\\' + path_id
+	# get the correct path
+	if platform == 'linux':
+		newpath = os.getcwd() + '/' + path_id
+	else:
+		newpath = os.getcwd() + '\\' + path_id
  
-    if not os.path.exists(newpath):
-        os.makedirs(newpath)                    # set up new directory with name of object
-
-    # os.chdir(newpath)
+	# set up new directory with the name of the object
+	if not os.path.exists(newpath):
+		os.makedirs(newpath)                    
 
     # create new data frame that will hold the information discovered
-    columns = ['name', 'period', 'epoch', 'minimum', 'maximum']
-    new_df = pd.DataFrame(columns=columns, index=range(0, 1))
+	columns = ['name', 'period', 'epoch', 'minimum', 'maximum']
+	new_df = pd.DataFrame(columns=columns, index=range(0, 1))
 
-    return new_df, newpath
+	return new_df, newpath
 
 
 def get_data_from_web(the_name):
-    '''Returns a .dat file from OGLE database as a dataframe'''
-    while True:    
-        # base = 'http://ogledb.astrouw.edu.pl/~ogle/photdb/data//'
-        base = 'http://ogledb.astrouw.edu.pl/~ogle/photdb/getobj.php?'
-        url = base + the_name
-        print('The url is ' + url)
-        r = requests.get(url)
-        html_doc = r.text
-        soup = BeautifulSoup(html_doc, 'lxml')
+	'''Takes the object name and returns a .dat file with photometry data from OGLE database as a dataframe'''
+
+	while True:    
+		base = 'http://ogledb.astrouw.edu.pl/~ogle/photdb/getobj.php?'
+		url = base + the_name
+		print('The url is ' + url)
+		r = requests.get(url)
+		html_doc = r.text
+		soup = BeautifulSoup(html_doc, 'lxml')
     
-        try:
-            table = soup.find('pre').contents[0]
-        except AttributeError:
-            print('***No good DIA photometry for this object***')
-            print('')
-            print('MOVING TO NEXT OBJECT...')
-            df = pd.DataFrame(columns=['1', '2', '3'], index=range(0, 1))
-            break
+		try:
+			table = soup.find('pre').contents[0]
 
-        long_str = table.split()
-        col = ['HJD', 'mag', 'mag_err', 'photometry_flag', 'frame_grade']
-        total = len(long_str)
-        rows = int(total / 5)
-        df = pd.DataFrame(columns=col, index=range(0, rows))    
+		except AttributeError:
+			print('***No good DIA photometry for this object***')
+			print('')
+			print('MOVING TO NEXT OBJECT...')
+			df = pd.DataFrame(columns=['1', '2', '3'], index=range(0, 1))
+			break
 
-        for row in range(0, rows):
-            for i in range(0, 5):
-                df.iloc[row][i] = long_str[row * 5 + i]
+		long_str = table.split()
+		col = ['HJD', 'mag', 'mag_err', 'photometry_flag', 'frame_grade']
+		total = len(long_str)
+		rows = int(total / 5)
+		df = pd.DataFrame(columns=col, index=range(0, rows))    
 
-        df = df.apply(pd.to_numeric, errors='ignore')
-        df.to_csv('RAW_DATA.csv')
+		for row in range(0, rows):
+			for i in range(0, 5):
+				df.iloc[row][i] = long_str[row * 5 + i]
+
+		df = df.apply(pd.to_numeric, errors='ignore')
+		df.to_csv('RAW_DATA.csv')
         
-        break
+		break
 
-    return df
+	return df
 
 
 if __name__ == '__main__':
