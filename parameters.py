61916e52afc869d25ca56ecc8f6c58fcf805efe6
Blake Crosby <ptsouth97@gmail.com>
2018-05-24 22:58:08 -0400

    added testing function to parameters.py

--- parameters.py
+++ parameters.py
@@ -1,44 +1,62 @@
+#!/usr/bin/python3
+
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 import pandas as pd
 
 
+def main():
+	'''main function for testing this module'''
+
+	df = pd.read_csv('test_dataframe.csv')
+	find_min(df, 2)
+	find_max(df, 2)
+
+
 def find_min(dtn, fully_auto):
-    '''Finds and returns the average value near eclipse minima to control for measurement error'''
+	'''Finds and returns the average value near eclipse minima to control for measurement error'''
 
-    dtn = dtn.loc[dtn.loc[:, 'Phase'] < 0.01]
-    dtn = dtn.loc[dtn.loc[:, 'Phase'] > -0.01]
-    avg = round(dtn['mag'].mean(), 2)
+	dtn = dtn.loc[dtn.loc[:, 'Phase'] < 0.01]
+	dtn = dtn.loc[dtn.loc[:, 'Phase'] > -0.01]
+	avg = round(dtn['mag'].mean(), 2)
 
-    fig, ax = plt.subplots()
-    ax.yaxis.set_major_formatter(FormatStrFormatter('%0.2f'))
-    plt.scatter(dtn['Phase'], dtn['mag'])
-    plt.gca().invert_yaxis()
-    plt.ylabel('Ic-mag')
-    plt.xlabel('Phase')
-    plt.title('Red line should appear at average of data points')
-    plt.axhline(y=avg, color='red')  # plot horizontal line at the average near minima
+	fig, ax = plt.subplots()
+	ax.yaxis.set_major_formatter(FormatStrFormatter('%0.2f'))
+	plt.scatter(dtn['Phase'], dtn['mag'])
+	plt.gca().invert_yaxis()
+	plt.ylabel('Ic-mag')
+	plt.xlabel('Phase')
+	plt.title('Red line should appear at average of data points')
+	plt.axhline(y=avg, color='red')  # plot horizontal line at the average near minima
 
-    plt.grid()
-    if fully_auto != '1':
-        plt.show()
+	plt.grid()
 
-    return avg
+	if fully_auto != '1':
+		plt.show()
+
+	return avg
 
 
 def find_max(dfx, full_auto):
-    dfx = dfx.loc[dfx.loc[:, 'Phase'] < 0.26]
-    dfx = dfx.loc[dfx.loc[:, 'Phase'] > 0.24]
-    av = round(dfx['mag'].mean(), 2)
+	'''finds the max'''
 
-    plt.scatter(dfx['Phase'], dfx['mag'])
-    plt.gca().invert_yaxis()
-    plt.ylabel('Ic-mag')
-    plt.xlabel('Phase')
-    plt.title('Red line should appear at average of data points')
-    plt.axhline(y=av, color='red')  # plot horizontal line at the average near minima
-    plt.grid()
-    if full_auto != '1':
-        plt.show()
+	dfx = dfx.loc[dfx.loc[:, 'Phase'] < 0.26]
+	dfx = dfx.loc[dfx.loc[:, 'Phase'] > 0.24]
+	av = round(dfx['mag'].mean(), 2)
 
-    return av
+	plt.scatter(dfx['Phase'], dfx['mag'])
+	plt.gca().invert_yaxis()
+	plt.ylabel('Ic-mag')
+	plt.xlabel('Phase')
+	plt.title('Red line should appear at average of data points')
+	plt.axhline(y=av, color='red')  # plot horizontal line at the average near minima
+	plt.grid()
+
+	if full_auto != '1':
+		plt.show()
+
+	return av
+
+
+if __name__ == '__main__':
+	main()
